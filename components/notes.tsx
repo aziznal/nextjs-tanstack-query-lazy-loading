9ba469c2ec354a5289cf7dac0715b8c8afc251cf
Aziz <aziznal.dev@gmail.com>
2024-02-04 14:02:17 +0300

    create notes component

---
+++ components/notes.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link";
+
+export default function Notes() {
+  return (
+    <div className="w-full border mt-12 rounded-md text-xs p-2">
+      <h2 className="font-bold">Notes</h2>
+
+      <ul className="list-decimal pl-4 flex flex-col gap-1 pt-2">
+        <li>
+          Read the{" "}
+          <Link
+            href="https://tanstack.com/query/latest/docs/framework/react/guides/infinite-queries"
+            target="_blank"
+          >
+            docs
+          </Link>
+        </li>
+
+        <li>
+          Lazy loading is a <i>client-side</i> illusion. {`It's`} just sending
+          requests for the next data as the user progresses in the app.
+        </li>
+
+        <li>
+          page means a bunch of items, not a page in the traditional sense.
+        </li>
+
+        <li>page size is the number of items per page.</li>
+
+        <li>
+          Having an API that supports the parameters necesssary for lazy loading
+          makes a lot of work trivial.
+        </li>
+
+        <li>
+          {`Tanstack's`} <code>useInfiniteQuery</code> is a great tool for this
+          on the client.
+        </li>
+
+        <li>
+          API receives:
+          <ul className="list-disc pl-4 pt-1">
+            <li>
+              <code>cursor</code> (a.k.a offset) the starting point of the next
+              page
+            </li>
+
+            <li>
+              <code>count</code> (a.k.a pageSize) how many items to return
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          API sends back:
+          <ul className="list-disc pl-4 pt-1">
+            <li>
+              <code>items</code> list for the requested page
+            </li>
+
+            <li>
+              <code>nextCursor</code> to be used in the next request if there is
+              a next page, otherwise <code>null</code> or <code>undefined</code>
+            </li>
+
+            <li>
+              <code>totalItems</code> (optional) to know when to stop requesting
+              more items on the client
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          Client stores all pages received so far and appends new pages to that
+          list when new pages are fetched.
+        </li>
+
+        <li>
+          Always let the user feel they know {`what's`} happening (with loading
+          indicators, for example)
+        </li>
+
+        <li>
+          Showing some sort of indication of the max items helps ground the{" "}
+          {`"infinity"`}
+          of the situation, improving UX.
+        </li>
+      </ul>
+    </div>
+  );
+}
